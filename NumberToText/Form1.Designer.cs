9db5f2e88b74809b170792d3311d2da32f43dde9
Jeff Luya Torririt <jeff@onepac.net>
2018-04-24 00:59:50 +0800

    Add project files.

---
+++ NumberToText/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace NumberToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblWordedNumber = new System.Windows.Forms.Label();
+            this.lblFormatedNumber = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(12, 12);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(361, 26);
+            this.txtNum.TabIndex = 0;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(12, 60);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(361, 38);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert Whole Number To Text";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblWordedNumber
+            // 
+            this.lblWordedNumber.AutoSize = true;
+            this.lblWordedNumber.Location = new System.Drawing.Point(8, 139);
+            this.lblWordedNumber.Name = "lblWordedNumber";
+            this.lblWordedNumber.Size = new System.Drawing.Size(51, 20);
+            this.lblWordedNumber.TabIndex = 2;
+            this.lblWordedNumber.Text = "label1";
+            // 
+            // lblFormatedNumber
+            // 
+            this.lblFormatedNumber.AutoSize = true;
+            this.lblFormatedNumber.Location = new System.Drawing.Point(8, 113);
+            this.lblFormatedNumber.Name = "lblFormatedNumber";
+            this.lblFormatedNumber.Size = new System.Drawing.Size(51, 20);
+            this.lblFormatedNumber.TabIndex = 3;
+            this.lblFormatedNumber.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(379, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Whole Number Only";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 359);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFormatedNumber);
+            this.Controls.Add(this.lblWordedNumber);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblWordedNumber;
+        private System.Windows.Forms.Label lblFormatedNumber;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NumberToText/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NumberToText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private Dictionary<int, string> less20;
+        private Dictionary<int, string> multiple10;
+        private Dictionary<int, string> multiple100;
+
+        private string RemoveDecimal(string number)
+        {
+            if(number.IndexOf('.') > -1)
+            {
+                return number.Split('.')[0].ToString();
+            }
+            return number;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            lblWordedNumber.Text = string.Empty;
+            lblFormatedNumber.Text = string.Empty;
+
+            long.TryParse(RemoveDecimal(txtNum.Text), out long num);
+            
+            string numStr = $"{num:#,###,##0}";
+            lblFormatedNumber.Text += numStr;
+
+            string[] numArray = numStr.Split(',');
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                string word = $"{WholeNumberToWords(int.Parse(numArray[i]))}";
+                word += word != string.Empty ? $" {multiple100[numArray.Length - i]} " : string.Empty;
+                lblWordedNumber.Text += word;
+            }
+        }
+
+        private string WholeNumberToWords(int num)
+        {
+            if (num > 0 && num < 20)
+            {
+                return less20[num];
+            }
+            else if (num > 0 && num < 100)
+            {
+                return $"{multiple10[num / 10]} {WholeNumberToWords(num % 10)}";
+            }
+            else if (num > 99)
+            {
+                return $"{less20[num / 100]} Hundred {WholeNumberToWords(num % 100)}";
+            }
+
+            return string.Empty;
+        }
+
+        private void Init()
+        {
+            less20 = new Dictionary<int, string>();
+            less20.Add(1, "One");
+            less20.Add(2, "Two");
+            less20.Add(3, "Three");
+            less20.Add(4, "Four");
+            less20.Add(5, "Five");
+            less20.Add(6, "Six");
+            less20.Add(7, "Seven");
+            less20.Add(8, "Eight");
+            less20.Add(9, "Nine");
+            less20.Add(10, "Ten");
+            less20.Add(11, "Eleven");
+            less20.Add(12, "Twelve");
+            less20.Add(13, "Thirteen");
+            less20.Add(14, "Fourteen");
+            less20.Add(15, "Fifteen");
+            less20.Add(16, "Sixteen");
+            less20.Add(17, "Seventeen");
+            less20.Add(18, "Eighteen");
+            less20.Add(19, "Nineteen");
+
+            multiple10 = new Dictionary<int, string>();
+            multiple10.Add(2, "Twenty");
+            multiple10.Add(3, "Thirty");
+            multiple10.Add(4, "Forty");
+            multiple10.Add(5, "Fifty");
+            multiple10.Add(6, "Sixty");
+            multiple10.Add(7, "Seventy");
+            multiple10.Add(8, "Eighty");
+            multiple10.Add(9, "Ninety");
+
+            multiple100 = new Dictionary<int, string>();
+            multiple100.Add(1, "");
+            multiple100.Add(2, "Thousand");
+            multiple100.Add(3, "Million");
+            multiple100.Add(4, "Billion");
+            multiple100.Add(5, "Trillion");
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Init();
+            lblFormatedNumber.Text = string.Empty;
+            lblWordedNumber.Text = string.Empty;
+        }
+    }
+
+   
+}
+
